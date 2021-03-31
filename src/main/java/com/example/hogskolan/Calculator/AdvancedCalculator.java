02511a6cafb4a4d0e0e0c50b680b272aaa9de45c
Jolanta Barkauskaite <barkauskaitejolanta@gmail.com>
2021-03-31 12:08:51 +0200

    updated code

--- src/main/java/com/example/hogskolan/Calculator/AdvancedCalculator.java
+++ src/main/java/com/example/hogskolan/Calculator/AdvancedCalculator.java
@@ -6,48 +6,51 @@ import java.lang.Math. *;
 
 public class AdvancedCalculator implements AdvancedOperations {
 
-	
+	//this is operation of square by simply multiplying value by itself 
 	public double square(double number) {
 		double result = number * number;
 		return result;
 	}
-
+	//this is operation of raising the number by the power of x number 
 	public double raisToThePowerOf(double base, double exponent) {
 		double result = Math.pow(base, exponent);
 		return result;
 	}
-	//needs testing, add if stmnt
+
 	//to find out sine of number x, first application needs to convert it to radians 
-	//by dividing it to 180 and then multiplying it by PI.
+	//by dividing it to 180 and then multiplying it by PI with already exsisting java formula
 	public double sinFunction(double sin) {
-		if (sin>=-360&&sin<360) {
-			double result = Math.round((sin/180)* Math.PI);
+		//if (sin>=-360&&sin<360) {
+			double angleInDegrees = sin; 
+			double result = Math.sin(Math.toRadians(angleInDegrees));
 			return result;
-		}else {
+		/*}else {
 			System.out.println("Please enter the correct angle value between -360 and 360.");
 			return 0.0;
-		}	
+		}	*/
 	}
-	//needs testing 
+	//The arcsin function is defined between -1 and 1 so it's base suppose to be between that range
 	public double inverseSinFunction(double asin) {
-		if (asin>=-1 && asin<=1) {
+		//if (asin>=-1 && asin<=1) {
 			double result = Math.asin(asin);
 			return result;
-		}else {
-			System.out.println("The argument value is NaN or absolute value is greater than 1.");
+		/*}else {
+			System.out.println("The argument value is NaN or absolute value is out of base range x[-1; 1].");
 			return 0.0;
-		}
+		}*/
 	}
 
-	//needs testing
+	//Natural logarithm base is e, where e is an irretional and transcendetal number
+	//approximately 2,718281828459. So the exponent will be a given number which java formula
+	//will automaticly calculate and return value. The exponent value must be x>0.
 	public double naturalLogarithm(double natLog) {
-		if(natLog>0) {
+		//if(natLog>0) {
 			double result = Math.log(natLog);
 			return result;
-		}else {
+		/*}else {
 			System.out.println("The real natural logarithm function ln(x) is defined only when x>0");
 			return 0.0;
-		}
+		}*/
 		
 		
 	}
--- src/main/java/com/example/hogskolan/Main/Main.java
+++ src/main/java/com/example/hogskolan/Main/Main.java
@@ -1,10 +1,12 @@
 package com.example.hogskolan.Main;
 
+import com.example.hogskolan.Calculator.AdvancedCalculator;
 import com.example.hogskolan.Calculator.BasicCalculator;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		BasicCalculator basicCalc = new BasicCalculator();
 		//System.out.println(basicCalc.addition(2.0, 3.0));
 		//System.out.println(basicCalc.addition(-2.0, 3.0));
@@ -15,8 +17,31 @@ public class Main {
 		//System.out.println(basicCalc.multiplication(2.0, 3.0));
 		//System.out.println(basicCalc.multiplication(-2.0, 3.0));
 		
-		System.out.println(basicCalc.devision(6.0, 3.0));
-		System.out.println(basicCalc.devision(2.0, 4.0));
+		//System.out.println(basicCalc.devision(6.0, 3.0));
+		//System.out.println(basicCalc.devision(2.0, 4.0));
+		
+		AdvancedCalculator advancedCalc = new AdvancedCalculator();
+		
+		//System.out.println(advancedCalc.square(2));
+		//System.out.println(advancedCalc.square(-2));
+		
+		//System.out.println(advancedCalc.raisToThePowerOf(2, 3));
+		//System.out.println(advancedCalc.raisToThePowerOf(2, -3));
+		
+		//System.out.println(advancedCalc.sinFunction(30));
+		//System.out.println(advancedCalc.sinFunction(380));
+		//System.out.println(advancedCalc.sinFunction(-20));
+		
+		//System.out.println(advancedCalc.inverseSinFunction(-1));
+		//System.out.println(advancedCalc.inverseSinFunction(1.5));
+		//System.out.println(advancedCalc.inverseSinFunction(0));
+		//System.out.println(advancedCalc.inverseSinFunction(-3));
+		
+		
+		//System.out.println(advancedCalc.naturalLogarithm(-3));
+		//System.out.println(advancedCalc.naturalLogarithm(10));
+		//System.out.println(advancedCalc.naturalLogarithm(0));
+		//System.out.println(advancedCalc.naturalLogarithm(0.5));
 		
 	}
 
